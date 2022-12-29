d60f2dc52c02dd7d77e9fd8fdf263a3837b9e3f9
Akhmadullo-Shokirov <akhmadullo2001@outlook.com>
2022-12-29 12:44:00 +0100

    First commit

---
+++ WebCustomerTracker/src/com/adlo/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.adlo.springdemo.dao;
+
+import java.util.List;
+
+import com.adlo.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
